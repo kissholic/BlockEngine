11ae31e682826bc7451f3690c20f0e7a83ddcdf6
kissholic <kissholicovo@outlook.com>
2024-08-23 23:46:44 +0800

    WIP

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -5,8 +5,23 @@
  */
 
 
+#include <filesystem>
+#include "glad/glad.h"
+#define GLFW_INCLUDE_OPENGL
+#include "GLFW/glfw3.h"
 #include <spdlog/spdlog.h>
 #include "Graphics/GraphicsContext.h"
+#include "Graphics/ShaderLoader.h"
+#include "Path.h"
+
+#include "BEConfig.h"
+
+
+float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	0.5f, -0.5f, 0.0f,
+	0.0f, 0.5f, 0.0f
+};
 
 
 int main(int, char**)
@@ -18,7 +33,32 @@ int main(int, char**)
 		return -1;
 	}
 
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+
+	std::filesystem::path vertexPath = be::Path::ShaderPath() / "DummyVertex.glsl";
+	std::filesystem::path fragShaderPath = be::Path::ShaderPath() / "DummyFrag.glsl";
+	auto shaderProgram = be::CreateShaderProgram(vertexPath, fragShaderPath);
+
+	shaderProgram.Use();
+	glBindVertexArray(VBO);
+
 	while (!Context.ShouldExit()) {
+		glClearColor(0.3f, 0.6f, 0.5f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		shaderProgram.Use();
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		Context.Step(1.0 / 60.0);
 	}
 
